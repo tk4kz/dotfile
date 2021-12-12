eccd8ad7add0148b88e04fa68f3a5106f099b6c2
Takeshi kz Ohira <tk4kz@yawaragi-thai.com>
2021-12-13 08:22:25 +0900

    markdown.elのmarkdown-code-faceを調整

--- init.el
+++ init.el
@@ -161,10 +161,20 @@
    markdown-command "pandoc -t html5")
 
   (set-face-attribute 'markdown-code-face nil
-                      :inherit 'default)
-  (set-face-attribute 'markdown-inline-code-face nil
-                      :inherit 'default
-                      :foreground (face-attribute font-lock-type-face :foreground))
+                      :inherit 'outline-1)
+  (set-face-attribute 'markdown-pre-face nil
+                      :inherit 'outline-1)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-code-face ((t (:inherit outline-1))))
+ '(markdown-pre-face ((t (:inherit outline-1)))))
+
+
+
   
 ;;; hydra-markdown
 (defhydra hydra-markdown (:hint nil :exit t)
